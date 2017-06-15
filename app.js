fbd29e85639baee1d44f6a2bd9b205487b96f7ee
Mateusz Bosek <bosek.mateusz@gmail.com>
2017-06-15 12:34:01 +0200

    Display img from api

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+const API_URL = 'https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo';
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').catch(function(error) {
-    console.log('Registration failed with ' + error);
-  }
-)}
+  navigator.serviceWorker.register('sw.js')
+    .catch(error => console.log('Registration failed with ' + error));
+}
+
+async function setPhotoURL() {
+    try {
+        const response = await fetch(API_URL);
+        const json = await response.json();
+        return json.url;
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+async function setImageURL(){
+    let myImage = document.querySelector('img');
+    const url = await setPhotoURL();
+    myImage.src = url
+}
+
+setImageURL();
